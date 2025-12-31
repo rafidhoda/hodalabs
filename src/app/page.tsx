80c50b3a3192e73f73c8a442f82377e6ec13a6a1
Rafid Hoda <rafidhoda@gmail.com>
2025-12-31 18:26:01 +0100

    Just for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-col items-center gap-8 text-center">
         <h1 className="text-5xl font-bold tracking-tight text-black dark:text-zinc-50">
-          Hoda Labs
+          Hoda Labs Company
         </h1>
         <UserAuth />
       </main>
